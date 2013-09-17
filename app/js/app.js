6a79cd5637326251f9f35265f63dedee89f8f027
baya <kayak.jiang@gmail.com>
2013-09-17 23:10:54 +0800

    ng SearchStarsCtl

--- app/js/app.js
+++ app/js/app.js
@@ -4,7 +4,7 @@
 // Declare app level module which depends on filters, and services
 angular.module('gStar', ['gStar.filters', 'gStar.services', 'gStar.directives', 'gStar.controllers']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/stars', {templateUrl: 'partials/partial1.html', controller: 'StarsCtrl'});
+    $routeProvider.when('/', {templateUrl: 'partials/stars.html', controller: 'StarsCtrl'});  
+    $routeProvider.when('/stars', {templateUrl: 'partials/stars.html', controller: 'StarsCtrl'});
     $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-    $routeProvider.otherwise({redirectTo: '/stars'});
   }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,19 @@
 /* Controllers */
 
 angular.module('gStar.controllers', []).
-  controller('StarsCtrl10', [function() {
+    controller('StarsCtrl', [function() {
+    }])
+    .controller('SearchStarsCtrl', ['$scope', function($scope){
+	$scope.q = '';
+	$scope.submit = function(){
+	    if (this.q){
+		alert(this.q);
+	    } else {
+		alert('OK');
+	    }
+	    
+	}
+    }])
+    .controller('MyCtrl2', [function() {
 
-  }])
-  .controller('MyCtrl2', [function() {
-
-  }]);
+    }]);
