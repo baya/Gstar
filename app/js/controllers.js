44a795522542326f820e99a3b09ad4986b334e7d
baya <kayak.jiang@gmail.com>
2013-09-19 00:12:01 +0800

    q

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,6 +3,14 @@
 /* Controllers */
 
 angular.module('gStar.controllers', []).
+    controller('SearchFormCtrl', ['$scope', '$location', function($scope, $location){
+	$scope.q = '';
+	$scope.submit = function(){
+	    if(this.q){
+		$location.path('stars').search({q: this.q});
+	    }
+	};
+    }]).
     controller('StarsCtrl', ['$scope', '$routeParams', 'SearchStars', function($scope, $routeParams, SearchStars){
 	$scope.stars = SearchStars.get({q: $routeParams.q});
     }]).
