cb0f93c749806d485696b444a2f93f1a15351e9f
baya <kayak.jiang@gmail.com>
2013-09-19 23:37:07 +0800

    search stars by name and description

--- ground/config/sets.rb
+++ ground/config/sets.rb
@@ -3,6 +3,6 @@ Ground do
   set :env, (ENV['RACK_ENV'] || 'development').to_s
   set :db_info, YAML.load_file(File.join(root, 'ground/config', 'database.yml'))[env]
   set :db, Sequel.connect(db_info)
-  set :log_file, File.join(root, "logs", "#{env}.log")
+  set :log_file, File.join(root, "ground/logs", "#{env}.log")
   set :logger, ::Logger.new(log_file)
 end
--- ground/states/stars/index.rb
+++ ground/states/stars/index.rb
@@ -1,10 +1,18 @@
 module Stars
   class Index < Ground::State
 
+    set :db, Ground.db
+
     def call
-      res = GithubAPI::ListStarredRepos()
-      json res
+      stars = find_stars(params[:q])
+      json stars.all.to_json
     end
-    
+
+    private
+
+    def find_stars(q)
+      db[:stars].grep([:name, :description], "%#{q}%")
+    end
+
   end
 end
