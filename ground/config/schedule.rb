d91146e605421e18294da02e1c0f0555624e08e7
jiangguimin <jiangguimin@funguide.com.cn>
2013-09-26 10:26:56 +0800

    update

--- ground/config/schedule.rb
+++ ground/config/schedule.rb
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 job_type :rake_utf8, "export LANG=zh_CN.UTF-8 && cd :path && :environment_variable=:environment bundle exec rake :task --silent :output"
-set :output, "/Users/jim/Projects/Gstar/ground/logs/cron_log.log"
+
+set :output, File.expand_path(File.join(File.dirname(__FILE__), '../', 'logs', "cron_log.log"))
 every 1.minutes do
   rake_utf8 "cron:check_latest_stars"
 end
