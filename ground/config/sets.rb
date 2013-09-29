5cfe302bda5e95075e1f63bb8a95084e16568f55
baya <kayak.jiang@gmail.com>
2013-09-30 01:05:53 +0800

    以rackup -D启动时，搜索不出结果

--- ground/config/sets.rb
+++ ground/config/sets.rb
@@ -1,7 +1,9 @@
 Ground do
   set :root, File.expand_path(File.dirname(File.dirname(File.dirname(__FILE__))))
   set :env, (ENV['RACK_ENV'] || 'development').to_s
-  set :db_info, YAML.load_file(File.join(root, 'ground/config', 'database.yml'))[env]
+  db_info = YAML.load_file(File.join(root, 'ground/config', 'database.yml'))[env]
+  db_info['database'] = File.expand_path(File.join(root, 'ground', db_info['database']))
+  set :db_info, db_info
   set :db, Sequel.connect(db_info)
   set :log_file, File.join(root, "ground/logs", "#{env}.log")
   set :logger, ::Logger.new(log_file)
