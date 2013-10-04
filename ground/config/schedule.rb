4c0d99c152472a867b4ad26662ad80223e1a2013
baya <kayak.jiang@gmail.com>
2013-10-04 13:41:01 +0800

    ubuntu test

--- ground/config/schedule.rb
+++ ground/config/schedule.rb
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-job_type :rake_utf8, "export LANG=zh_CN.UTF-8 && cd :path && :environment_variable=:environment bundle exec rake :task --silent :output"
+job_type :rake_utf8, "export LANG=en_US.UTF-8 && cd :path && :environment_variable=:environment bundle exec rake :task --silent :output"
 
 set :output, File.expand_path(File.join(File.dirname(__FILE__), '../', 'logs', "cron_log.log"))
 every 1.minutes do
